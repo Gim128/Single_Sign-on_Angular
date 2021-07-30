3498491269f6e422184fadcc558b160ee1139a75
Gim128 <gimhanpabasara4@gmail.com>
2021-07-30 15:27:19 +0530

    init login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {JwksValidationHandler, OAuthService} from "angular-oauth2-oidc";
+import {authConfig} from "./app.config";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'signin';
+
+  constructor(private oauthService:OAuthService) {
+    this.configuresignin();
+  }
+
+  configuresignin(){
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndLogin();
+  }
+
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
 }
